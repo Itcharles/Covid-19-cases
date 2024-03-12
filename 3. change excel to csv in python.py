4112a919e6371a4b0c8212b754e9ef93fade176b
Itcharles <124773954+Itcharles@users.noreply.github.com>
2024-03-12 20:50:12 +0100

    Update 3. change excel to csv in python.py

--- 3. change excel to csv in python.py
+++ 3. change excel to csv in python.py
@@ -4,6 +4,10 @@ import pandas as pd
 df1 = pd.read_excel('C:\\ Your directory \\Covid cases.xlsx')
 df2 = pd.read_excel('C:\\ Your directory \\Covid vaccinations.xlsx')
 
+# We need to change population column to float type in Covid cases.xlsx otherwise we will no be able to load the data to SQL:
+if 'population' in df.columns:
+  df['population'] = df['population'].astype('float')
+
 # Saving files in csv format.
 df1.to_csv('Covid cases csv.csv', index=False
 df2.to_csv('Covid vacinnations csv.csv', index=False           
